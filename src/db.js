36b6a016526a46bd71fdcda9fa73040c27934004
Nanguan Lin <nanguanlin6@gmail.com>
2024-10-12 22:11:56 -0500

    Add workflow file (#2)
    * Add workflow file

--- src/db.js
+++ src/db.js
@@ -2,11 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
   ssl: {
     rejectUnauthorized: false,
   },
